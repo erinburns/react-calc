6e6f259ccc3919bcb859310bf202e9c75cebbab6
erinb52s <erinb52s@gmail.com>
2017-11-21 15:33:28 +0000

    set up webpack style loader

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
 import React from 'react'; // import React package
 import ReactDOM from 'react-dom'; // import ReactDOM package
+import './styles/reset.css'; // import css reset file
+import './styles/main.css'; // import external css file
+
+
 
 // using the render method, we will mount this node into our DOM (html file) on the element with 
 // id of 'app'
